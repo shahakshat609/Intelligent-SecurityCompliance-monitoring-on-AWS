6766fb7f05db935021be2245008f9d83cd984ed6
Akshat Shah <akshatshah@Akshat-Shahs-MacBook-Pro.local>
2019-04-06 21:55:26 -0700

    get_security_groups_API

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,50 @@
 var express=require('express');
+var AWS=require('aws-sdk');
 var app=express();
 
-
+AWS.config.update({region:'us-west-2'});
 app.get('/',(req,res)=>{
     res.send({hi:"there"});
 })
+var paarms={};
+var ec2=new AWS.EC2();
+var list_security_groups=[];
+let get_security_groups=function(){ec2.describeInstances(paarms,function(err,data){
+    console.log("In get_security_groups func")
+    if(err) 
+    console.log(err);
+    else
+    reservations=data.Reservations
+    //console.log(reservations);
+    reservations.forEach(function(reservation){
+        //console.log(instance.Instances);
+        reservation.Instances.forEach(function(instance){
+        //console.log(instance.SecurityGroups);  
+        instance.SecurityGroups.forEach(function(security_group){
+            
+                list_security_groups.push(security_group.GroupId);
+        })
+    });
+    });
 
+    
+});
+}
+
+// var promise=new Promise(function(resolve,reject){
+//     if(list_security_groups.length>0)
+//     resolve(list_security_groups);
+//     else
+//     reject("Did not get security groups")
+//     return promise
+// })
+
+function temp(){
+    console.log("Starting the method get_sec-groups")
+    get_security_groups()
+    console.log(list_security_groups);
+}
+temp()
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT);
